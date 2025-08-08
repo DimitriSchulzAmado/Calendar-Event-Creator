cac1ee0880ea43819d2d3f2090d44866320d2781
“Dimitri” <dimitri.s.amado@gmail.com>
2025-08-08 19:25:40 -0300

    :bug: fix: Refactor webhook data handling to extract properties for event creation

--- main.py
+++ main.py
@@ -8,15 +8,17 @@ app = Flask(__name__)
 @app.route("/notion-webhook/", methods=["POST"])
 def receive_notion_webhook():
     if request.method == "POST":
-        data = request.get_json()
-        print("Received data:", data)
-        # GoogleCalendar.create_event(
-        #     "teste",
-        #     "Localização",
-        #     "Descrição",
-        #     datetime.datetime(2025, 8, 1, 10, 0),
-        #     datetime.datetime(2025, 8, 1, 11, 0),
-        # )
+        response = request.get_json()
+        properties = response["data"]["properties"]
+
+        GoogleCalendar.create_event(
+            summary=properties["Name"]["title"][0]["text"]["content"],
+            location=properties["Place"]["rich_text"][0]["text"]["content"],
+            description="None",
+            start_time=properties["Date"]["date"]["start"],
+            end_time=properties["Date"]["date"]["end"],
+        )
+
         return jsonify({"status": "success", "message": "Webhook received"})
     else:
         return jsonify({"status": "error", "message": "Invalid request method"}), 405
