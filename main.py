ffbd108be33e32e3076ec1b8e2907ce34ca24ec3
“Dimitri” <dimitri.s.amado@gmail.com>
2025-08-08 14:18:25 -0300

    fix: Update webhook data handling and improve response structure

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import datetime
+import requests
 from google_calendar.google_calendar_service import GoogleCalendar
 from flask import Flask, request, jsonify
 
@@ -8,8 +8,8 @@ app = Flask(__name__)
 @app.route("/notion-webhook/", methods=["POST"])
 def receive_notion_webhook():
     if request.method == "POST":
-        data = request.json
-        print(data)
+        data = request.get_json()
+        print("Received data:", data)
         # GoogleCalendar.create_event(
         #     "teste",
         #     "Localização",
@@ -17,7 +17,7 @@ def receive_notion_webhook():
         #     datetime.datetime(2025, 8, 1, 10, 0),
         #     datetime.datetime(2025, 8, 1, 11, 0),
         # )
-        return jsonify({"status": "success", "message": "Webhook received"}), 200
+        return jsonify({"status": "success", "message": "Webhook received"})
     else:
         return jsonify({"status": "error", "message": "Invalid request method"}), 405
 
